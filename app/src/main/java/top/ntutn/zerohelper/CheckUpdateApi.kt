c48e61a7ad463c530cffdc9eb049272b49751fc4
liuhaixin.zero <liuhaixin.zero@bytedance.com>
2021-11-29 16:05:53 +0800

    proguard adaption

--- app/src/main/java/top/ntutn/zerohelper/CheckUpdateApi.kt
+++ app/src/main/java/top/ntutn/zerohelper/CheckUpdateApi.kt
@@ -1,16 +1,25 @@
 package top.ntutn.zerohelper
 
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
 import retrofit2.http.GET
 
+@Keep
 data class ApkElement(
+    @SerializedName("versionCode")
     val versionCode: Long,
+    @SerializedName("versionName")
     val versionName: String,
-    val outputFile: String
+    @SerializedName("outputFile")
+    val outputFile: String,
 )
 
+@Keep
 data class ApkMetaData(
+    @SerializedName("elements")
     val elements: List<ApkElement>)
 
+@Keep
 interface CheckUpdateApi {
     @GET("/zerofancy/zerohelper/releases/download/latest/output-metadata.json")
     suspend fun getApkMetaData(): ApkMetaData
